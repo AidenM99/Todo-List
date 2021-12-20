2d9de7fe1cdd326adda63ffb329c9acb957fb9f4
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-12-20 12:01:12 +0000

    Create modal for making new todo items

---
+++ src/modules/CreateTask.js
@@ -0,0 +1,8 @@
+export default class Task {
+  constructor(task, description, dueDate, priority) {
+    this.task = task;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,7 +1,11 @@
+import Task from "./CreateTask";
+
 export default function initialiseUI() {
   initSidebar();
   initModal();
 }
+
+const tasks = [];
 
 function initSidebar() {
   const sidebarItems = document.querySelectorAll(".sidebar-item");
@@ -16,15 +20,20 @@ function initSidebar() {
 }
 
 function initModal() {
-  const newTodo = document.querySelector(".newTodo");
-  const closeButton = document.querySelector(".close-button");
-
+  const newTodo = document.querySelector(".new-todo");
   newTodo.addEventListener("click", openModal);
+
+  const closeButton = document.querySelector(".close-button");
   closeButton.addEventListener("click", closeModal);
+
+  const addTask = document.querySelector(".add-task");
+  addTask.addEventListener("click", newTask);
+
   document.addEventListener("click", (event) => {
     if (
       !event.target.closest(".modal-content") &&
-      !event.target.classList.contains("newTodo")
+      !event.target.classList.contains("new-todo") &&
+      !event.target.classList.contains("add-task")
     ) {
       closeModal();
     }
@@ -41,4 +50,25 @@ function closeModal() {
   const modal = document.querySelector(".modal-bg");
 
   modal.style.display = "none";
+}
+
+function newTask() {
+  const taskInput = document.getElementById("task");
+  const task = taskInput.value;
+
+  const descriptionInput = document.getElementById("description");
+  const description = descriptionInput.value;
+
+  const dateInput = document.getElementById("due-date");
+  const date = dateInput.value;
+
+  const priorityInput = document.getElementById("priority");
+  const priority = priorityInput.value;
+
+  if (!task || !description || !date) return;
+
+  const addTask = new Task(task, description, date, priority);
+
+  tasks.push(addTask);
+  console.log(tasks);
 }
